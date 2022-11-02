3b5e2b8f90946bdb7a4a13d1b633c9ba87e3d090
DeLapasse Cecile <cecile.de-lapasse@telecomnancy.eu>
2022-11-02 23:41:19 +0100

    Analyse de la commande demandée

--- src/ftc_test.c
+++ src/ftc_test.c
@@ -3,8 +3,78 @@
 
 //fonctions qui correspondent aux commandes (bien si c'est du même nom)
 
+void test(){
+    return;
+}
+void name(char* parametre){
+    return;
+}
+void size(char* parametre){
+    return;
+}
+void date(char* parametre){
+    return;
+}
+void mime(char* parametre){
+    return;
+}
+void ctc(char* parametre){
+    return;
+}
+void dir(char* parametre){
+    return;
+}
 
+void commande_a_exec(char* commande,char* parametre){
+    char* commandes[]={'-test','-name','-size','-date','-mime','-ctc','-dir'};
 
+    long int taille = sizeof(commandes)/sizeof(commandes[0]);
+
+    int commande_exec;
+
+    printf("Size tableau : %ld\n", taille);
+
+    for (int i = 0 ; i < taille ; i++){
+        if (strcmp(commande,commandes[i])){
+            commande_exec = i;
+        }
+    }
+
+    switch (commande_exec)
+    {
+    case 0:
+        test();
+        break;
+    
+    case 1:
+        name(parametre);
+        break;
+
+    case 2:
+        size(parametre);
+        break;
+
+    case 3:
+        date(parametre);
+        break;
+
+    case 4:
+        mime(parametre);
+        break;
+
+    case 5:
+        ctc(parametre);
+        break;
+
+    case 6:
+        dir(parametre);
+        break;
+
+    default:
+        printf("Erreur, commande non reconnue.\n");
+        break;
+    }
+}
 
 
 
@@ -12,26 +82,22 @@ int main(int argc, char const *argv[]){
     printf("Hello world!\n");
     char* chemin=".";//la racine de dossier, donné par question1
 
-    char* commandes[7]={'-test','-name','-size','-date','-mime','-ctc','-dir'};
-
-    char* options[7]; //mettre les options de chaque commande à l'indice i qui correspond bien à la commande i dans la liste commandes
+    char* parametres[7]; //mettre les options de chaque commande à l'indice i qui correspond bien à la commande i dans la liste commandes
     //plutôt faire une dictionnaire => pas de structure dictionnaire mais table de hashage... avec à chaque alvéole la liste simplement chaînée des options
+    //on utilise commande pour trouver les options dans le dico, si la commande != clé du dico, commande pas reconnue
+    //=> pas besoin car options possibles trop grandes pour dates et tailles => analyser le paramètre dans la fonction, si c'est sous une forme qui marche, ok
+    //ou sinon on met dans paramètres que les options qui sont uniques ou dénombrables, genre d m y pour dates mais pas chaque nb et + ou -
 
     char* starting_point = argv[1]; // d'où on commence la recherche
-    
-    char* commande = argv[2];
+    //printf("Départ : %s\n",starting_point);
+
+    char* commande = argv[2];   
     //printf("Commande : %s\n",commande);
 
-    for (int i = 0 ; i < 7 ; i++){
-        if (strcmp(commande,commandes[i])){
-            for (int j = 0 ; j < options[i].length ; j++){ //nb d'options de la commande
-                if (strcmp())
-            }//pour chaque option donnée dans la ligne de commande faut qu'il fasse ça...
-        }
-    }
-
-
-    //on utilise commande pour trouver les options dans le dico, si la commande != clé du dico, commande pas reconnue
+    char* parametre = argv[3]; //paramètre de la commande
+    //printf("Paramètre : %s\n",parametre);
+    
+    commande_a_exec(commande,parametre);
 
     parcourirDossier(chemin);
 
